3b5cbfc92077e296c5d28bf2baa35a912a97700e
f1amee-dev <filipgamesp@gmail.com>
2025-05-21 16:17:00 +0200

    fix tailwind border utility class issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,9 @@ export default {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      borderColor: {
+        DEFAULT: "hsl(var(--border))",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
